ae0bc38c7c787bd7a39d43f3243d2a53c6998e70
Jaime Calderon <jaimecalderon907@gmail.com>
2022-04-08 00:29:41 -0500

    actualizar script

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 13:12:23 2020
+
+@author: jaimecalderonocampo
+"""
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-
 from skimage import data, img_as_float
 from skimage import exposure
 from skimage import color, morphology, io
 
 plt.close('all')
-
-Ima = io.imread('Brain.png')
-# gris = color.rgb2gray(Ima)
+Brain = io.imread('Brain.png')
+# Brain = color.rgb2gray(Brain)
 plt.figure(1)
-plt.imshow(Ima, cmap='gray')
+plt.imshow(Brain, cmap='gray')
 plt.ion()
 
-tol = 0.2
-f, c = Ima.shape
+tol = 20
+X, Y = Brain.shape
 pos = np.int32(plt.ginput(0, 0))
-aux1 = np.zeros([f, c], dtype=np.byte)
-aux2 = np.zeros([f, c], dtype=np.byte)
+aux1 = np.zeros([X, Y], dtype=np.byte)
+aux2 = np.zeros([X, Y], dtype=np.byte)
 
 aux1[pos[:, 1], pos[:, 0]] = 1
-pixeles = Ima[pos[:, 1], pos[:, 0]]
+pixeles = Brain[pos[:, 1], pos[:, 0]]
 promedio = np.mean(pixeles)
 
 while (np.sum(aux1) != np.sum(aux2)):
@@ -29,12 +34,15 @@ while (np.sum(aux1) != np.sum(aux2)):
     aux2 = np.copy(aux1)
     bordes = morphology.binary_dilation(aux1) - aux1
     pos_borde = np.argwhere(bordes)
-    gris_bordes = Ima[pos_borde[:, 0], pos_borde[:, 1]]
+    gris_bordes = Brain[pos_borde[:, 1], pos_borde[:, 0]]
+
     compara = list(np.logical_and([gris_bordes > (promedio - tol)], [gris_bordes < (promedio + tol)]))
     datos = pos_borde[compara]
     aux1[datos[:, 0], datos[:, 1]] = 1
     plt.imshow(aux1, cmap='gray')
-    plt.pause(1)
+    plt.pause(0.01)
 
 
+plt.figure(2)
+plt.imshow(Brain, cmap='gray')
 
